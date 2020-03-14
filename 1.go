78713a0bc0e7ebf77e93cd35c980a90d5c9abbb8
mihailo <mihailovlajkovic98@gmail.com>
2020-03-14 18:30:29 +0100

    Changed variable names to english

--- 1.go
+++ 1.go
@@ -2,32 +2,26 @@ package main
 
 import "fmt"
 
-type Domina struct{
-	levi int
-	desni int
-
+type Dominoe struct{
+	left int
+	right int
 }
 
 func main(){
-	var mapaDomina = make(map[int]*Domina)
-	
-	var brojac int
-	brojac = 0
-
+	var dominoesMap = make(map[int]*Dominoe)
+	var counter int
+	counter = 0
 	for i:=0; i < 7; i++{
 		for j:=i; j < 7; j++{
-			mapaDomina[brojac] = new (Domina)
-			mapaDomina[brojac].levi = i
-			mapaDomina[brojac].desni = j
-			brojac++
+			dominoesMap[counter] = new (Dominoe)
+			dominoesMap[counter].left = i
+			dominoesMap[counter].right = j
+			counter++
 		}
-	} 
-	
-	//mapaDomina[0] = new (Domina)
-	//mapaDomina[0].levi = 5
+	}
 
-	for i:=0; i < brojac; i++{
-		fmt.Print(*mapaDomina[i], "   ")
 
+	for i:=0; i < counter; i++{
+		fmt.Print(*dominoesMap[i], "   ")
 	}
 }
