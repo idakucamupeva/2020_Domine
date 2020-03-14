36a3052b3b2497e363d224bb7f515e228169e65c
mihailo <mihailovlajkovic98@gmail.com>
2020-03-14 19:17:53 +0100

    Updated

--- 1.go
+++ 1.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+
 
 type Dominoe struct{
 	left int
@@ -11,6 +17,11 @@ func main(){
 	var dominoesMap = make(map[int]*Dominoe)
 	var counter int
 	counter = 0
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	broj := r1.Intn(28)
+	var player1 []int
+	player1 = append(player1,broj)
 	for i:=0; i < 7; i++{
 		for j:=i; j < 7; j++{
 			dominoesMap[counter] = new (Dominoe)
@@ -24,4 +35,5 @@ func main(){
 	for i:=0; i < counter; i++{
 		fmt.Print(*dominoesMap[i], "   ")
 	}
+	fmt.Print(player1[0])
 }
