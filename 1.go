cb1aeca0c8720646e825b962be0039c926050df6
stefandur <stefandur@hotmail.com>
2020-03-14 17:04:34 +0100

    finally we have some part of code

--- 1.go
+++ 1.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type Domina struct{
+	levi int
+	desni int
+
+}
+
 func main(){
-	fmt.Println("Hello, world!")
+	var mapaDomina = make(map[int]*Domina)
+	
+	var brojac int
+	brojac = 0
+
+	for i:=0; i < 7; i++{
+		for j:=i; j < 7; j++{
+			mapaDomina[brojac] = new (Domina)
+			mapaDomina[brojac].levi = i
+			mapaDomina[brojac].desni = j
+			brojac++
+		}
+	} 
+	
+	//mapaDomina[0] = new (Domina)
+	//mapaDomina[0].levi = 5
+
+	for i:=0; i < brojac; i++{
+		fmt.Print(*mapaDomina[i], "   ")
+
+	}
 }
